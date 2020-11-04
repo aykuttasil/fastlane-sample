938264eda39d65cdafe95a28682a9d3a7604c5af
Aykut Asil <aykuttasil@gmail.com>
2020-11-04 12:34:31 +0300

    screengrab plugin

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -1,20 +1,55 @@
 package com.example.myapplication
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.junit.runners.JUnit4
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(JUnit4::class)
 class ExampleInstrumentedTest {
+
+    // JVMField needed!
+    @Rule
+    @JvmField
+    val localeTestRule = LocaleTestRule()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java, false, false)
+
+    @Test
+    fun testTakeScreenshot() {
+        activityRule.launchActivity(null)
+        //1
+        Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+
+        Espresso.onView(ViewMatchers.withId(R.id.askButton))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        //2
+        Screengrab.screenshot("beforeFabClick")
+
+        //3
+        Espresso.onView(ViewMatchers.withId(R.id.askButton)).perform(ViewActions.click())
+
+        //4
+        Screengrab.screenshot("afterFabClick")
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
